0af335485a80e6e69a784cd2ee3dd97a307132b4
Marian Clements <marian.clements@fluxfederation.com>
2019-09-26 11:49:43 +1200

    SUM Allow Characters to be created and managed by larps

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,50 +4,49 @@ class CharactersController < ApplicationController
   # GET /characters
   # GET /characters.json
   def index
+    @larp = Larp.find(params['larp_id'])
     @characters = Character.all
   end
 
   # GET /characters/1
   # GET /characters/1.json
   def show
+    @larp = Larp.find(params['larp_id'])
   end
 
   # GET /characters/new
   def new
+    @larp = Larp.find(params['larp_id'])
     @character = Character.new
   end
 
   # GET /characters/1/edit
   def edit
+    @larp = Larp.find(params['larp_id'])
   end
 
   # POST /characters
   # POST /characters.json
   def create
     @character = Character.new(character_params)
+    @character.larp_id = params['larp_id']
 
-    respond_to do |format|
-      if @character.save
-        format.html { redirect_to @character, notice: 'Character was successfully created.' }
-        format.json { render :show, status: :created, location: @character }
-      else
-        format.html { render :new }
-        format.json { render json: @character.errors, status: :unprocessable_entity }
-      end
+    if @character.save
+      redirect_to larp_characters_path(params['larp_id']), notice: 'Character was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /characters/1
   # PATCH/PUT /characters/1.json
   def update
-    respond_to do |format|
-      if @character.update(character_params)
-        format.html { redirect_to @character, notice: 'Character was successfully updated.' }
-        format.json { render :show, status: :ok, location: @character }
-      else
-        format.html { render :edit }
-        format.json { render json: @character.errors, status: :unprocessable_entity }
-      end
+    @character.larp_id = params['larp_id']
+
+    if @character.update(character_params)
+      redirect_to larp_characters_path(params['larp_id']), notice: 'Character was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -55,20 +54,17 @@ class CharactersController < ApplicationController
   # DELETE /characters/1.json
   def destroy
     @character.destroy
-    respond_to do |format|
-      format.html { redirect_to characters_url, notice: 'Character was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to larps_characters_path(@larp), notice: 'Character was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_character
-      @character = Character.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_character
+    @character = Character.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def character_params
-      params.fetch(:character, {})
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def character_params
+    params.fetch(:character, {}).permit(:name, :short_description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   root to: "larps#index"
-  
+
   resources :goals
   resources :items
-  resources :characters
-  resources :larps
+
+  resources :larps do
+    resources :characters
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
